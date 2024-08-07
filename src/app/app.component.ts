2562affb90e4a6af90f16d296498e919a9ee7be5
DenysDubas <planetgreen74@gmail.com>
2024-08-07 13:00:48 +0300

    implemented main logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators  } from '@angular/forms';
+
+const todos = [
+  {id: 1, title: 'HTML + CSS', completed: true},
+  {id: 2, title: 'JS', completed: false},
+  {id: 3, title: 'React', completed: false},
+  {id: 4, title: 'Vue.js', completed: false},
+];
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  editing = false;
+  todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ],
+    }),
+  })
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  get activeTodos() {
+    return this.todos.filter(todo =>!todo.completed);
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value,
+      completed: false,
+    }
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
